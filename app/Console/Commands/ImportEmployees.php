e78286de5579bfc9fb7bb791608822d0aeea0a6b
Alan Lira <alan_lirasp@hotmail.com>
2017-07-26 00:09:32 -0300

    started command to import employees

--- app/Console/Commands/ImportEmployees.php
+++ app/Console/Commands/ImportEmployees.php
@@ -3,6 +3,10 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Employee;
+use App\Company;
+use Illuminate\Support\Facades\Storage;
+use Excel;
 
 class ImportEmployees extends Command
 {
@@ -37,6 +41,55 @@ class ImportEmployees extends Command
      */
     public function handle()
     {
-        //
+        $files = Storage::files("files_to_be_imported");
+        
+        if(count($files) > 0) {
+            foreach($files as $file) {
+                $data = Excel::load(Storage::url('app/'.$file), function($reader) {
+			    })->get()->toArray();
+
+                if(!empty($data) && count($data) > 0) {
+                    foreach ($data as $key => $value) {
+                        try {
+                            if($value[4] == 'I') {
+                                $this->info("iniciando importação");
+                                if(Company::find($value[0]) == null) {
+                                    throw new \Exception("Empresa {$value[0]} não encontrada");
+                                }
+                                $this->info("empresa encontrada");
+
+                                $employee = new Employee();
+                                $employee->id = $value[1];
+                                $employee->company_id = $value[0];
+                                $employee->name = $value[2];
+                                $employee->processed_at = $this->convertDate($value[3]);
+                                $employee->status = 1;
+                                $employee->save();
+                            }
+                            else if($value[4] == 'E') {
+                                $employee = Employee::find($value[1]);
+
+                                if($employee == null) {
+                                    throw new \Exception("Funcionário {$value[1]} não encontrado");
+                                }
+
+                                $employee->status = 0;
+                                $employee->save();
+                            }
+                        }
+                        catch(\Exception $error) {
+                            $this->info($error->getMessage());
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function convertDate($dateString)
+    {
+        $parts = explode("/", $dateString);
+        return $parts[2] . '-' . $parts[1] . '-' . $parts[0];
     }
 }
